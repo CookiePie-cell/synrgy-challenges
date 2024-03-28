4209fcd112f35b8ad3f3f45b16dcec2efbbdf8ff
Julio Nicholas <juliojnicolas1@gmail.com>
2024-03-28 15:54:55 +0800

    refactor project structure

---
+++ ChallengeOne/ChallengeOne/src/main/java/org/example/OrderItem.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class OrderItem {
+    private String item;
+    private Integer quantity;
+
+    public OrderItem(String item, Integer quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
